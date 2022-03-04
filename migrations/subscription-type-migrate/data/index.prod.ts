1af9b80cf27938c9cbb88b3aa4ecbeae5e2fbaba
ManojPandey121 <me.nose121@gmail.com>
2022-03-04 15:12:47 +0545

    [FLO-12]: added subscription  data for migration

---
+++ migrations/subscription-type-migrate/data/index.prod.ts
@@ -0,0 +1,12 @@
+import { subscriptionTypes } from '.';
+import { STAFF, SUPER_ADMIN } from '../constants';
+
+const acceptedSubsctiptions = [SUPER_ADMIN, STAFF];
+export const subscriptionTypesProd = subscriptionTypes.filter(
+  (subscriptionType) =>
+    acceptedSubsctiptions.some(
+      (subscriptionTypeIdentifier) =>
+        subscriptionTypeIdentifier ===
+        subscriptionType.subscriptionTypeIdentifier,
+    ),
+);
---
+++ migrations/subscription-type-migrate/interfaces/subscription.interface.ts
@@ -0,0 +1,8 @@
+import { Document, Types } from 'mongoose';
+
+export interface ISubscription extends Document {
+  _id: Types.ObjectId;
+  subscriptionTypeIdentifier: string;
+  subscriptionType: string;
+  position: number;
+}
