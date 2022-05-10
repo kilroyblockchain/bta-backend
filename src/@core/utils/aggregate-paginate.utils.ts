9a4482e62efef27b1f7ecbb2f35c4d6a9747f52b
Bhagat Grg <bhagattamu@gmail.com>
2022-05-10 16:21:27 +0545

    [FLO-208] Added user search with organization unit and staffname also

--- src/@core/utils/aggregate-paginate.utils.ts
+++ src/@core/utils/aggregate-paginate.utils.ts
@@ -1,4 +1,4 @@
-import { PipelineStage } from 'mongoose';
+import { PaginateResult, PipelineStage } from 'mongoose';
 
 export interface IOptions {
     page: number;
@@ -12,6 +12,52 @@ export interface IFinalResult {
     page: number;
     pages: number;
 }
+
+/**
+ * Returns aggregate stage to paginate aggregate result
+ *
+ * @param {number} page - Page Number
+ * @param {number} limit - Limit of data in page
+ * @returns {Array<any>}  Array of stages to give paginate Result from aggregation in index 0
+ */
+export const getPaginateDocumentStage = (page: number, limit: number): Array<any> => [
+    {
+        $facet: {
+            metadata: [{ $count: 'total' }, { $addFields: { page: page, limit: limit } }],
+            docs: [{ $skip: (page - 1) * limit }, { $limit: limit }]
+        }
+    },
+    {
+        $unwind: '$metadata'
+    },
+    {
+        $addFields: {
+            total: '$metadata.total',
+            page: '$metadata.page',
+            limit: '$metadata.limit'
+        }
+    },
+    {
+        $project: {
+            metadata: 0
+        }
+    }
+];
+
+/**
+ * Return builded data with docs, limit and total
+ *
+ * @param paginateResult - Pagination created by getPaginateDocumentStage function
+ * @returns {PaginateResult<T>}  Always return the paginate attributes i.e. docs, limit and total
+ */
+export const buildPaginateResult = <T>(paginateResult: PaginateResult<T>): PaginateResult<T> => {
+    return {
+        docs: paginateResult?.docs ?? [],
+        page: paginateResult?.page ?? 1,
+        limit: paginateResult?.limit ?? 10,
+        total: paginateResult?.total ?? 0
+    };
+};
 
 export const getFinalPaginationResult = (result: any, options: IOptions): IFinalResult => {
     let finalResult: IFinalResult = {
@@ -85,6 +131,56 @@ export const populateField = (collectionName: string, localField: string, foreig
         $unwind: {
             path: '$' + localField,
             preserveNullAndEmptyArrays: true
+        }
+    }
+];
+
+/**
+ * Return populate stage to populate field and get only selected fields in documents
+ *
+ * @param {string} collectionName - Collection to join
+ * @param {string} localField - Field from the input documents
+ * @param {string} foreignField - Field from the documents of the collection to be joined
+ * @param {string} select - Document Fields to be projected seperated by space
+ * @returns {Array<any>}  Populate stage and select stage
+ */
+export const populateFieldWithSelect = (collectionName: string, localField: string, foreignField: string, select = ''): Array<any> => [
+    {
+        $lookup: {
+            from: collectionName,
+            let: { [localField]: '$' + localField },
+            pipeline: [
+                {
+                    $match: {
+                        $expr: {
+                            $eq: ['$' + foreignField, '$$' + localField]
+                        }
+                    }
+                },
+                ...getProjectStageFromSelect(select)
+            ],
+            as: localField
+        }
+    },
+    {
+        $unwind: {
+            path: '$' + localField,
+            preserveNullAndEmptyArrays: true
+        }
+    }
+];
+
+/**
+ * Return sort stage that sort documents by propertName
+ *
+ * @param {string} propName - Property name
+ * @param {string} type - 'ASC' or 'DESC' -> Default = 'ASC' i.e. 1
+ * @returns {Array<PipelineStage>}  Sort stage
+ */
+export const sortDocumentsBy = (propName: string, type = 'ASC'): Array<PipelineStage> => [
+    {
+        $sort: {
+            [propName]: type === 'ASC' ? 1 : -1
         }
     }
 ];
