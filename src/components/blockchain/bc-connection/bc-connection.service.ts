d245dda578dd400feaa28f3616336bdd8639f2d5
jenish-kilroy <jenish@kilroyblockchain.com>
2022-06-03 21:32:51 +0545

    [FLO-274]: Fix channel mapping not found issue by adding user on BC when staffing changes on user update

--- src/components/blockchain/bc-connection/bc-connection.service.ts
+++ src/components/blockchain/bc-connection/bc-connection.service.ts
@@ -27,8 +27,8 @@ export class BcConnectionService {
             }
             const err = error as AxiosError;
             logger.error(err.response ? JSON.stringify(err.response.data) : err);
-            if (err.response && err.response.status == HttpStatus.CONFLICT && err.response.data && err.response.data.detail) {
-                throw new HttpException(err.response.data.detail, err.response.status);
+            if (err.response && err.response.status == HttpStatus.CONFLICT && err.response.data) {
+                throw new HttpException(err.response.data, err.response.status);
             } else {
                 throw new InternalServerErrorException();
             }
