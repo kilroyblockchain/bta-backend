8867dbedb4f0aeb5537c9d06aa1756c80b6f5c29
surajthaqurie <surajchan68@gmail.com>
2022-07-06 16:36:53 +0545

    [BB-71]: adding default organization and staffing of verfiy company admin

---
+++ src/@utils/events/interfaces/organization.interface.ts
@@ -0,0 +1,9 @@
+export interface ICompanyAdminOrganizationPayload {
+    companyId: string;
+    bcNodeInfo: string;
+    channels: string[];
+    bucketUrl: string;
+    organizationName: string;
+    staffingType: string;
+    userId: string;
+}
