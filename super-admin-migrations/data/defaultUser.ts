58c1b33863371d22f1e7b324e9f2bf50cd315545
surajthaqurie <surajchan68@gmail.com>
2022-07-06 21:59:01 +0545

    [BB-73]: updating super admin migration with bc key and .env file

--- super-admin-migrations/data/defaultUser.ts
+++ super-admin-migrations/data/defaultUser.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { Types } from 'mongoose';
 import { DEFAULT_COMPANY_ID, DEFAULT_SUPER_ADMIN_ID, STAFFING_UNIT_ID } from 'super-admin-migrations/constants/default-constant';
 
@@ -9,7 +10,7 @@ export const admin =
         autoPassword: false,
         firstName: 'Suyog',
         lastName: 'Khanal',
-        email: 'suyog@kilroyblockchain.com',
+        email: 'suraj@kilroyblockchain.com',
         phone: '+1 (488) 431-4341',
         country: new Types.ObjectId('60e6fe33d27e2133c4855221'),
         state: new Types.ObjectId('60e6fe3dd27e2133c4856174'),
@@ -33,7 +34,8 @@ export const admin =
         verificationExpires: new Date(),
         blockExpires: new Date(),
         verification: 'ff2fc9c8-b8e8-484d-8e0d-b053d1945ebe',
-        resetLink: ''
+        resetLink: '',
+        bcSalt: process.env.SUPER_ADMIN_BC_SALT
     };
 
 export const adminCompany = {
