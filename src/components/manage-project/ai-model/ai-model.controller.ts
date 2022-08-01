117614035a8ee11ac2e852c730883222e31c8e2b
surajthaqurie <surajchan68@gmail.com>
2022-08-01 16:47:39 +0545

    [BB-110]: Get single experiment latest oracle bc hash

--- src/components/manage-project/ai-model/ai-model.controller.ts
+++ src/components/manage-project/ai-model/ai-model.controller.ts
@@ -241,13 +241,13 @@ export class AiModelController {
     @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: COMMON_ERROR.UNAUTHORIZED })
     @ApiResponse({ status: HttpStatus.FORBIDDEN, description: COMMON_ERROR.FORBIDDEN })
     @ApiResponse({ status: HttpStatus.NOT_FOUND, description: MANAGE_PROJECT_CONSTANT.VERSION_RECORD_NOT_FOUND })
-    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: MANAGE_PROJECT_CONSTANT.UNABLE_TO_GET_ALL_EXPERIMENT_ORACLE_BC_HASH })
-    @ApiResponse({ status: HttpStatus.OK, schema: { example: { data: '6416ff97b7d18b402f3697ad0bbc43a67a2c5961f7b7909e906ed0a118c7b840' } }, description: MANAGE_PROJECT_CONSTANT.GOT_ALL_EXPERIMENT_ORACLE_BC_HASH_SUCCESS })
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: MANAGE_PROJECT_CONSTANT.UNABLE_TO_GET_LOG_FILE_ORACLE_BC_HASH })
+    @ApiResponse({ status: HttpStatus.OK, schema: { example: { data: '6416ff97b7d18b402f3697ad0bbc43a67a2c5961f7b7909e906ed0a118c7b840' } }, description: MANAGE_PROJECT_CONSTANT.GOT_LOG_FILE_ORACLE_BC_HASH_SUCCESS })
     async getLogFileOracleBcHash(@Param('id') versionId: string): Promise<FLOResponse> {
         try {
-            return new FLOResponse(true, [MANAGE_PROJECT_CONSTANT.GOT_ALL_EXPERIMENT_ORACLE_BC_HASH_SUCCESS]).setSuccessData(await this.aiModelService.getLogFileOracleBcHash(versionId)).setStatus(HttpStatus.OK);
+            return new FLOResponse(true, [MANAGE_PROJECT_CONSTANT.GOT_LOG_FILE_ORACLE_BC_HASH_SUCCESS]).setSuccessData(await this.aiModelService.getLogFileOracleBcHash(versionId)).setStatus(HttpStatus.OK);
         } catch (err) {
-            throw new BadRequestException(MANAGE_PROJECT_CONSTANT.UNABLE_TO_GET_ALL_EXPERIMENT_ORACLE_BC_HASH, err);
+            throw new BadRequestException(MANAGE_PROJECT_CONSTANT.UNABLE_TO_GET_LOG_FILE_ORACLE_BC_HASH, err);
         }
     }
 
@@ -330,11 +330,28 @@ export class AiModelController {
     }
 
     @Get('log-experiment-oracle-bc-hash/:id')
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(PermissionGuard)
+    @Permission(ACCESS_TYPE.READ)
+    @Feature(FEATURE_IDENTIFIER.MODEL_VERSION)
+    @Roles(ROLE.STAFF, ROLE.OTHER)
+    @ApiBearerAuth()
+    @ApiHeader({
+        name: 'Bearer',
+        description: 'The token we need for auth'
+    })
+    @ApiOperation({ summary: 'Get single experiment latest oracle bc hash' })
+    @ApiParam({ name: 'id', required: true, description: 'version Id' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: COMMON_ERROR.UNAUTHORIZED })
+    @ApiResponse({ status: HttpStatus.FORBIDDEN, description: COMMON_ERROR.FORBIDDEN })
+    @ApiResponse({ status: HttpStatus.NOT_FOUND, description: MANAGE_PROJECT_CONSTANT.VERSION_RECORD_NOT_FOUND })
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: MANAGE_PROJECT_CONSTANT.UNABLE_TO_GET_SINGLE_EXPERIMENT_ORACLE_BC_HASH })
+    @ApiResponse({ status: HttpStatus.OK, schema: { example: { data: '1905182e52b6b2da25cbeef11cd38e75349aa072bc2a2ddb934e0eca50d61acb' } }, description: MANAGE_PROJECT_CONSTANT.GOT_SINGLE_EXPERIMENT_ORACLE_BC_HASH_SUCCESS })
     async getExperimentOracleBcHash(@Param('id') experimentId: string): Promise<FLOResponse> {
         try {
             return new FLOResponse(true, [MANAGE_PROJECT_CONSTANT.GOT_ALL_EXPERIMENT_DETAILS_SUCCESS]).setSuccessData(await this.aiModelService.getExperimentOracleBcHash(experimentId)).setStatus(HttpStatus.OK);
         } catch (err) {
-            throw new BadRequestException(MANAGE_PROJECT_CONSTANT.UNABLE_TO_GET_ALL_EXPERIMENT_DETAILS, err);
+            throw new BadRequestException(MANAGE_PROJECT_CONSTANT.GOT_SINGLE_EXPERIMENT_ORACLE_BC_HASH_SUCCESS, err);
         }
     }
 
