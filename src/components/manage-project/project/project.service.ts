d236f96c0b4cc8e75c530e477350a19df2d7f1ea
Suraj <suraj@kilroyblockchain.com>
2022-11-03 14:48:55 +0545

    [BB-164]: added error logger in project service

--- src/components/manage-project/project/project.service.ts
+++ src/components/manage-project/project/project.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ConflictException, NotFoundException, forwardRef, Inject } from '@nestjs/common';
+import { Injectable, ConflictException, NotFoundException, forwardRef, Inject, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Request } from 'express';
 import { Model, PaginateModel, PaginateResult } from 'mongoose';
@@ -25,143 +25,203 @@ export class ProjectService {
     ) {}
 
     async createNewProject(newProject: CreateProjectDto, req: Request): Promise<IProject> {
-        const user = req['user']._id;
-        const companyId = getCompanyId(req);
+        const logger = new Logger(ProjectService.name + '-createNewProject');
+        try {
+            const user = req['user']._id;
+            const companyId = getCompanyId(req);
 
-        const isProjectUnique = await this.isProjectUnique(newProject.name, companyId);
-        if (!isProjectUnique) {
-            throw new ConflictException(MANAGE_PROJECT_CONSTANT.PROJECT_NAME_CONFLICT);
-        }
-        for (const userId of newProject.members) {
-            const user = await this.userModel.findById(userId).select('_id');
-            if (!user) {
-                throw new NotFoundException(USER_CONSTANT.USER_NOT_FOUND);
+            const isProjectUnique = await this.isProjectUnique(newProject.name, companyId);
+            if (!isProjectUnique) {
+                throw new ConflictException(MANAGE_PROJECT_CONSTANT.PROJECT_NAME_CONFLICT);
             }
-        }
-        const project = new this.projectModel(newProject);
-        project.members = newProject.members;
-        project.createdBy = user;
-        project.companyId = companyId;
+            for (const userId of newProject.members) {
+                const user = await this.userModel.findById(userId).select('_id');
+                if (!user) {
+                    throw new NotFoundException(USER_CONSTANT.USER_NOT_FOUND);
+                }
+            }
+            const project = new this.projectModel(newProject);
+            project.members = newProject.members;
+            project.createdBy = user;
+            project.companyId = companyId;
 
-        await this.projectBcService.createBcProject(req, project);
-        return await project.save();
+            await this.projectBcService.createBcProject(req, project);
+            return await project.save();
+        } catch (err) {
+            logger.error(err);
+            throw err;
+        }
     }
 
     async isProjectUnique(name: string, companyId: string): Promise<boolean> {
-        const project = await this.projectModel.findOne({ name: { $regex: name, $options: 'i' }, companyId });
-        if (project) {
-            return false;
+        const logger = new Logger(ProjectService.name + '-isProjectUnique');
+        try {
+            const project = await this.projectModel.findOne({ name: { $regex: name, $options: 'i' }, companyId });
+            if (project) {
+                return false;
+            }
+            return true;
+        } catch (err) {
+            logger.error(err);
+            throw err;
         }
-        return true;
     }
 
     async getAllProject(req: Request): Promise<PaginateResult<IProject>> {
-        const companyId = getCompanyId(req);
+        const logger = new Logger(ProjectService.name + '-getAllProject');
+        try {
+            const companyId = getCompanyId(req);
 
-        const userId = req['user']._id;
-        const { page = 1, limit = 10, status = true, search, searchValue } = req.query;
-        const searchQuery = search && search === 'true' && searchValue ? getSearchFilterWithRegexAll(searchValue.toString(), ['name', 'details', 'domain', 'purpose']) : {};
-        const options = {
-            populate: [
-                { path: 'members', select: 'firstName lastName email address phone' },
-                { path: 'createdBy', select: 'firstName lastName email' },
-                {
-                    path: 'projectVersions',
-                    match: {
-                        $or: [{ createdBy: userId.toString(), versionStatus: VersionStatus.DRAFT }, { versionStatus: { $ne: VersionStatus.DRAFT } }]
-                    },
-                    populate: {
-                        path: 'createdBy',
-                        select: 'firstName lastName'
+            const userId = req['user']._id;
+            const { page = 1, limit = 10, status = true, search, searchValue } = req.query;
+            const searchQuery = search && search === 'true' && searchValue ? getSearchFilterWithRegexAll(searchValue.toString(), ['name', 'details', 'domain', 'purpose']) : {};
+            const options = {
+                populate: [
+                    { path: 'members', select: 'firstName lastName email address phone' },
+                    { path: 'createdBy', select: 'firstName lastName email' },
+                    {
+                        path: 'projectVersions',
+                        match: {
+                            $or: [{ createdBy: userId.toString(), versionStatus: VersionStatus.DRAFT }, { versionStatus: { $ne: VersionStatus.DRAFT } }]
+                        },
+                        populate: {
+                            path: 'createdBy',
+                            select: 'firstName lastName'
+                        }
                     }
-                }
-            ],
-            lean: true,
-            limit: Number(limit),
-            page: Number(page),
-            sort: { updatedAt: -1 }
-        };
-        return await this.projectModel.paginate({ status, ...searchQuery, companyId }, options);
+                ],
+                lean: true,
+                limit: Number(limit),
+                page: Number(page),
+                sort: { updatedAt: -1 }
+            };
+            return await this.projectModel.paginate({ status, ...searchQuery, companyId }, options);
+        } catch (err) {
+            logger.error(err);
+            throw err;
+        }
     }
 
     async getProjectById(id: string, req: Request): Promise<IProject> {
-        const companyId = getCompanyId(req);
-        const project = await this.projectModel.findOne({ _id: id, companyId });
-        if (project) {
-            return project;
+        const logger = new Logger(ProjectService.name + '-getProjectById');
+        try {
+            const companyId = getCompanyId(req);
+            const project = await this.projectModel.findOne({ _id: id, companyId });
+            if (project) {
+                return project;
+            }
+            throw new NotFoundException(MANAGE_PROJECT_CONSTANT.PROJECT_RECORDS_NOT_FOUND);
+        } catch (err) {
+            logger.error(err);
+            throw err;
         }
-        throw new NotFoundException(MANAGE_PROJECT_CONSTANT.PROJECT_RECORDS_NOT_FOUND);
     }
 
     async updateProject(id: string, updateProject: CreateProjectDto, req: Request): Promise<IProject> {
-        const userId = req['user']._id;
+        const logger = new Logger(ProjectService.name + '-updateProject');
+        try {
+            const userId = req['user']._id;
 
-        const project = await this.getProjectById(id, req);
-        const companyId = getCompanyId(req);
+            const project = await this.getProjectById(id, req);
+            const companyId = getCompanyId(req);
 
-        if (!project) throw new NotFoundException([MANAGE_PROJECT_CONSTANT.PROJECT_RECORDS_NOT_FOUND]);
-        const isProjectUnique = await this.isProjectUnique(updateProject.name, companyId);
-        if (!isProjectUnique && project.name !== updateProject.name) {
-            throw new ConflictException(MANAGE_PROJECT_CONSTANT.PROJECT_NAME_CONFLICT);
+            if (!project) throw new NotFoundException([MANAGE_PROJECT_CONSTANT.PROJECT_RECORDS_NOT_FOUND]);
+            const isProjectUnique = await this.isProjectUnique(updateProject.name, companyId);
+            if (!isProjectUnique && project.name !== updateProject.name) {
+                throw new ConflictException(MANAGE_PROJECT_CONSTANT.PROJECT_NAME_CONFLICT);
+            }
+
+            const updatedProject = await this.projectModel.findOneAndUpdate({ _id: project._id }, updateProject, { new: true });
+            updatedProject.updatedBy = userId;
+
+            await this.projectBcService.createBcProject(req, updatedProject);
+            return await updatedProject.save();
+        } catch (err) {
+            logger.error(err);
+            throw err;
         }
-
-        const updatedProject = await this.projectModel.findOneAndUpdate({ _id: project._id }, updateProject, { new: true });
-        updatedProject.updatedBy = userId;
-
-        await this.projectBcService.createBcProject(req, updatedProject);
-        return await updatedProject.save();
     }
 
     async deleteProject(id: string, req: Request): Promise<IProject> {
-        const companyId = getCompanyId(req);
-        return await this.projectModel.findOneAndUpdate({ _id: id, companyId }, { status: false }, { new: true });
+        const logger = new Logger(ProjectService.name + '-deleteProject');
+        try {
+            const companyId = getCompanyId(req);
+            return await this.projectModel.findOneAndUpdate({ _id: id, companyId }, { status: false }, { new: true });
+        } catch (err) {
+            logger.error(err);
+            throw err;
+        }
     }
 
     async enableProject(id: string, req: Request): Promise<IProject> {
-        const companyId = getCompanyId(req);
-        return await this.projectModel.findOneAndUpdate({ _id: id, companyId }, { status: true }, { new: true });
+        const logger = new Logger(ProjectService.name + '-enableProject');
+        try {
+            const companyId = getCompanyId(req);
+            return await this.projectModel.findOneAndUpdate({ _id: id, companyId }, { status: true }, { new: true });
+        } catch (err) {
+            logger.error(err);
+            throw err;
+        }
     }
 
     async addProjectPurpose(id: string, req: Request, file: Express.Multer.File, modelPurpose: AddProjectPurposeDto): Promise<IProject> {
-        const project = await this.getProjectById(id, req);
-        if (!project) throw new NotFoundException([MANAGE_PROJECT_CONSTANT.PROJECT_RECORDS_NOT_FOUND]);
+        const logger = new Logger(ProjectService.name + '-addProjectPurpose');
+        try {
+            const project = await this.getProjectById(id, req);
+            if (!project) throw new NotFoundException([MANAGE_PROJECT_CONSTANT.PROJECT_RECORDS_NOT_FOUND]);
 
-        if (file) {
-            project.purpose.docName = file.originalname;
-            project.purpose.docURL = `project-purposeDoc/${file.filename}`;
+            if (file) {
+                project.purpose.docName = file.originalname;
+                project.purpose.docURL = `project-purposeDoc/${file.filename}`;
+            }
+
+            if (modelPurpose.purposeDoc === '') {
+                project.purpose.docName = '';
+                project.purpose.docURL = '';
+            }
+            project.purpose.text = modelPurpose.purpose;
+            project.domain = modelPurpose.domain;
+            project.details = modelPurpose.details;
+
+            await this.projectBcService.createBcProject(req, project);
+            return await project.save();
+        } catch (err) {
+            logger.error(err);
+            throw err;
         }
-
-        if (modelPurpose.purposeDoc === '') {
-            project.purpose.docName = '';
-            project.purpose.docURL = '';
-        }
-        project.purpose.text = modelPurpose.purpose;
-        project.domain = modelPurpose.domain;
-        project.details = modelPurpose.details;
-
-        await this.projectBcService.createBcProject(req, project);
-        return await project.save();
     }
 
     async canAddProject(req: Request): Promise<boolean> {
-        const currentCompanyUser = await this.userService.findAllUserOfOrganization(req);
+        const logger = new Logger(ProjectService.name + '-canAddProject');
+        try {
+            const currentCompanyUser = await this.userService.findAllUserOfOrganization(req);
 
-        const userStaffing = [];
-        currentCompanyUser.docs.map((user) => user.company.map((company) => company.staffingId.map((staffing) => userStaffing.push(staffing['staffingName']))));
+            const userStaffing = [];
+            currentCompanyUser.docs.map((user) => user.company.map((company) => company.staffingId.map((staffing) => userStaffing.push(staffing['staffingName']))));
 
-        const isAIEngineer = !!userStaffing.find((staffingName) => staffingName.toLowerCase().includes(PROJECT_USER.AI_ENGINEER.toLowerCase()));
-        const isMLOpsEngineer = !!userStaffing.find((staffingName) => staffingName.toLowerCase().includes(PROJECT_USER.MLOps_ENGINEER.toLowerCase()));
-        const isStakeholder = !!userStaffing.find((staffingName) => staffingName.toLowerCase().includes(PROJECT_USER.STAKEHOLDER.toLowerCase()));
+            const isAIEngineer = !!userStaffing.find((staffingName) => staffingName.toLowerCase().includes(PROJECT_USER.AI_ENGINEER.toLowerCase()));
+            const isMLOpsEngineer = !!userStaffing.find((staffingName) => staffingName.toLowerCase().includes(PROJECT_USER.MLOps_ENGINEER.toLowerCase()));
+            const isStakeholder = !!userStaffing.find((staffingName) => staffingName.toLowerCase().includes(PROJECT_USER.STAKEHOLDER.toLowerCase()));
 
-        if (isAIEngineer && isMLOpsEngineer && isStakeholder) return true;
-        return false;
+            if (isAIEngineer && isMLOpsEngineer && isStakeholder) return true;
+            return false;
+        } catch (err) {
+            logger.error(err);
+            throw err;
+        }
     }
 
     async checkVersionInProject(projectId: string, versionId: string): Promise<boolean> {
-        const versionInProject = await this.projectModel.findOne({ _id: projectId, projectVersions: versionId });
-        if (versionInProject) {
-            return true;
+        const logger = new Logger(ProjectService.name + '-checkVersionInProject');
+        try {
+            const versionInProject = await this.projectModel.findOne({ _id: projectId, projectVersions: versionId });
+            if (versionInProject) {
+                return true;
+            }
+            return false;
+        } catch (err) {
+            logger.error(err);
+            throw err;
         }
-        return false;
     }
 }
