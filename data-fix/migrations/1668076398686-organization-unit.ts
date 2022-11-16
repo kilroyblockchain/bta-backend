06f261cbfb8392d0679a8d95f026e1533a8e9d57
Suraj <suraj@kilroyblockchain.com>
2022-11-16 14:08:41 +0545

    [BB-183]: update data-fix migration only added isMigrated field if staffing name be admin

--- data-fix/migrations/1668076398686-organization-unit.ts
+++ data-fix/migrations/1668076398686-organization-unit.ts
@@ -2,14 +2,29 @@ import 'dotenv/config';
 import { consoleLogWrapper } from 'app-migrations/helper-func';
 import { IOrganizationUnitInterface } from 'src/components/flo-user/user-roles/organization-unit/interfaces/organization-unit.interface';
 import { OrganizationUnitSchema } from 'src/components/app-user/user-roles/organization-unit/schemas/organization-unit.schema';
+import { StaffingInterface } from 'src/components/app-user/user-roles/organization-staffing/interfaces/organization-staffing.interface';
+import { StaffingSchema } from 'src/components/app-user/user-roles/organization-staffing/schemas/organization-staffing.schema';
 import * as mongoose from 'mongoose';
 
 async function up(): Promise<void> {
     try {
         const OrganizationUnitModel = mongoose.model<IOrganizationUnitInterface>('OrganizationUnit', OrganizationUnitSchema);
+        const StaffingModel = mongoose.model<StaffingInterface>('Staffing', StaffingSchema);
+
         consoleLogWrapper('Adding isMigrated filed in all organization units');
 
-        const updateOrganizationUnit = await OrganizationUnitModel.updateMany({}, { $set: { isMigrated: true } });
+        const migratedOrganizationIds = await StaffingModel.find({ staffingName: { $regex: 'Admin', $options: 'i' } }).select('organizationUnitId');
+
+        if (!migratedOrganizationIds.length) {
+            consoleLogWrapper('No Organization units are migrated');
+        }
+        const organizationUnitIds = [];
+
+        for (const organizationUnitId of migratedOrganizationIds) {
+            organizationUnitIds.push(organizationUnitId.organizationUnitId);
+        }
+
+        const updateOrganizationUnit = await OrganizationUnitModel.updateMany({ _id: { $in: organizationUnitIds } }, { $set: { isMigrated: true } });
         if (updateOrganizationUnit) {
             consoleLogWrapper('New filed isMigrated added in all organization units successfully');
         } else {
